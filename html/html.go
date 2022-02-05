39331591de04ce58e27d777804921a6bc48b31d3
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-02-05 23:29:45 +0900

    Feature parse HTML package

---
+++ html/html.go
@@ -0,0 +1,101 @@
+package html
+
+import (
+	"bytes"
+	"io"
+	"net/url"
+
+	"golang.org/x/net/html"
+	"golang.org/x/net/html/atom"
+)
+
+var parseFns = make(map[string]func(node *html.Node) Node)
+
+type Node interface {
+	URL() (*url.URL, error)
+	String() string
+}
+
+type AnchorNode struct {
+	Href string
+	Text string
+}
+
+func (n *AnchorNode) URL() (*url.URL, error) {
+	return url.Parse(n.Href)
+}
+
+func (n *AnchorNode) String() string {
+	return n.Text + n.Href
+}
+
+type ImgNode struct {
+	Src string
+	Alt string
+}
+
+func (n *ImgNode) URL() (*url.URL, error) {
+	return url.Parse(n.Src)
+}
+
+func (n *ImgNode) String() string {
+	return n.Alt + n.Src
+}
+func Parse(r io.Reader) ([]Node, error) {
+	parseFns[atom.A.String()] = parseAnchorNode
+	parseFns[atom.Img.String()] = parseImgNode
+
+	node, err := html.Parse(r)
+	if err != nil {
+		return nil, err
+	}
+
+	ns := make([]Node, 0, 1000)
+	parseNodes(&ns, node)
+
+	return ns, nil
+}
+
+func parseNodes(ns *[]Node, node *html.Node) {
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.ElementNode {
+			if f, ok := parseFns[c.DataAtom.String()]; ok {
+				*ns = append(*ns, f(c))
+			}
+			parseNodes(ns, c)
+		}
+	}
+}
+
+func parseAnchorNode(node *html.Node) Node {
+	var buff bytes.Buffer
+	for c := node.FirstChild; c != nil; c = c.NextSibling {
+		if c.Type == html.TextNode {
+			buff.WriteString(c.Data)
+		}
+	}
+
+	var href string
+	for _, v := range node.Attr {
+		if v.Key == "href" {
+			href = v.Val
+			break
+		}
+	}
+
+	return &AnchorNode{Text: buff.String(), Href: href}
+}
+
+func parseImgNode(node *html.Node) Node {
+	var src, alt string
+	for _, v := range node.Attr {
+		if v.Key == "src" {
+			src = v.Val
+		}
+		if v.Key == "alt" {
+			alt = v.Val
+		}
+	}
+
+	return &ImgNode{Src: src, Alt: alt}
+}
