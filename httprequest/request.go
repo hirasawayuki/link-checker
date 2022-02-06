eeef67c17eafea023b9ee3895c192475ce179fdf
hirasawayuki <hirasawa.yuki@zaim.co.jp>
2022-02-06 13:59:47 +0900

    Feature limit requests using semaphore

--- httprequest/request.go
+++ httprequest/request.go
@@ -1,19 +1,37 @@
 package httprequest
 
 import (
+	"context"
 	"fmt"
 	"net/http"
 	"net/url"
+	"sync"
+	"time"
 
 	"github.com/hirasawayuki/link-checker/html"
 )
 
-type CheckResult struct {
-	AnchorResults []string
-	ImgResults    []string
+const (
+	defaultRequestSpeedMillisecond = 100
+	maxConnectionCount             = 10
+)
+
+type CheckResults struct {
+	AnchorResults []*Result
+	ImgResults    []*Result
+
+	mux sync.Mutex
 }
 
-func (cr *CheckResult) append(node html.Node, result string) {
+type Result struct {
+	Text   string
+	Status int
+}
+
+func (cr *CheckResults) append(node html.Node, result *Result) {
+	cr.mux.Lock()
+	defer cr.mux.Unlock()
+
 	switch v := node.(type) {
 	case *html.AnchorNode:
 		cr.AnchorResults = append(cr.AnchorResults, result)
@@ -24,8 +42,8 @@ func (cr *CheckResult) append(node html.Node, result string) {
 	}
 }
 
-func CheckPage(pageURL string) (*CheckResult, error) {
-	check := &CheckResult{}
+func CheckPage(pageURL string) (*CheckResults, error) {
+	check := &CheckResults{}
 	u, err := url.Parse(pageURL)
 	if err != nil {
 		fmt.Printf("[ERROR] Parse URL failed. Plese check page url. (url=%s)\n", pageURL)
@@ -45,6 +63,26 @@ func CheckPage(pageURL string) (*CheckResult, error) {
 		return nil, fmt.Errorf("[ERROR] Parse HTML failed. (err=%w)\n", err)
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	semaphore := make(chan struct{}, maxConnectionCount)
+	for i := 0; i < maxConnectionCount-1; i++ {
+		semaphore <- struct{}{}
+	}
+
+	go func() {
+		for {
+			select {
+			case <-time.After(defaultRequestSpeedMillisecond * time.Millisecond):
+				<-semaphore
+			}
+		}
+	}()
+
+	errCh := make(chan error, len(ns))
+
+	wg := sync.WaitGroup{}
 	for _, n := range ns {
 		url, err := n.URL()
 		if err != nil {
@@ -56,16 +94,49 @@ func CheckPage(pageURL string) (*CheckResult, error) {
 		if url.Scheme == "" {
 			url.Scheme = u.Scheme
 		}
-		resp, err := http.Get(url.String())
-		if err != nil {
-			return nil, fmt.Errorf("[ERROR] Request failed. url=%s, HTTP Status=%d\n", u, resp.StatusCode)
-		}
-		if resp.StatusCode == http.StatusOK {
-			check.append(n, fmt.Sprintf("✓ HTTP Status: %d URL: %s Text(alt): %s", resp.StatusCode, url, n))
-			continue
-		}
-		check.append(n, fmt.Sprintf("X HTTP Status: %d URL: %s Text(alt): %s", resp.StatusCode, url, n))
+
+		wg.Add(1)
+		go func(n html.Node) {
+			defer wg.Done()
+			semaphore <- struct{}{}
+			if err := checkStatus(ctx, url.String(), n, check); err != nil {
+				cancel()
+				errCh <- err
+			}
+		}(n)
+	}
+	wg.Wait()
+
+	close(errCh)
+	var errs []error
+	for err := range errCh {
+		errs = append(errs, err)
+	}
+
+	if len(errs) > 0 {
+		return nil, errs[0]
 	}
 
 	return check, nil
+}
+
+func checkStatus(ctx context.Context, url string, n html.Node, check *CheckResults) error {
+	fmt.Println("check status")
+	resp, err := http.Get(url)
+	if err != nil {
+		return fmt.Errorf("[ERROR] Request failed. url=%s, HTTP Status=%d\n", url, resp.StatusCode)
+	}
+
+	r := &Result{}
+	if resp.StatusCode == http.StatusOK {
+		r.Text = fmt.Sprintf("✓ HTTP Status: %d URL: %s Text(alt): %s", resp.StatusCode, url, n)
+		r.Status = resp.StatusCode
+		check.append(n, r)
+		return nil
+	}
+
+	r.Text = fmt.Sprintf("X HTTP Status: %d URL: %s Text(alt): %s", resp.StatusCode, url, n)
+	r.Status = resp.StatusCode
+	check.append(n, r)
+	return nil
 }
